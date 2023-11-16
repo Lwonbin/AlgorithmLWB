927740fd96d1aa608ce5e7d38d9ef4ac54c0295f
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2023-11-16 22:49:35 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 216 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] arr = new int[9];
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            arr[i] = scan.nextInt();
+            sum += arr[i];
+        }
+
+        for (int x = 0; x < 8; x++) {
+            for(int y= x+1; y<9; y++) {
+                if(sum - arr[x] - arr[y]==100){
+                    arr[x] = 0;
+                    arr[y] = 0;
+                    Arrays.sort(arr);
+                    for(int z=2; z<9;z++){
+                        System.out.println(arr[z]);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
