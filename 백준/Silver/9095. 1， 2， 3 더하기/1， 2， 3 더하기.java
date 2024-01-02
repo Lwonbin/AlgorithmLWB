963f09b0e6663ff6f35d8d15d999678d777afde9
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2024-01-02 14:02:42 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 140 ms, Memory: 15864 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int dp[] = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for(int j=4; j<11; j++){
+            dp[j] = dp[j-1] + dp[j-2] + dp[j-3];
+        }
+
+        for(int i=0; i<N; i++){
+            int n = Integer.parseInt(br.readLine());
+            bw.write(dp[n] + "\n");
+
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
