09b177660514098973085bfeae65fcf2b3357dab
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2024-07-01 14:27:41 +0900

    [level 1] Title: 둘만의 암호, Time: 1.25 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -3,32 +3,35 @@ class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
         StringBuilder sb = new StringBuilder();
-        Set<Character> skipSet = new HashSet<>();
-        
-        // Skip 문자열을 Set에 추가하여 검색을 빠르게 만듦
-        for (char c : skip.toCharArray()) {
-            skipSet.add(c);
+        List<Character> list = new ArrayList<>();
+        for(int i=0; i<skip.length(); i++){
+            list.add(skip.charAt(i));
         }
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+        
+        for(int i=0; i<s.length(); i++){
             int count = 0;
-            int p = 0;
-
-            // index만큼 이동
-            while (count < index) {
+            
+            char ch = s.charAt(i);
+            
+            while(count < index){
                 ch++;
-                if (ch > 'z') {
-                    ch -= 26;
+                
+                if(ch > 'z'){
+                ch-=26;
                 }
-                // skipSet에 포함되지 않는 문자만 count 증가
-                if (!skipSet.contains(ch)) {
+                
+                if(!list.contains(ch)){
                     count++;
                 }
+                
+                
             }
-            sb.append(ch);
+        sb.append(ch);
+            
         }
         answer = sb.toString();
+        
+        
         return answer;
     }
 }
