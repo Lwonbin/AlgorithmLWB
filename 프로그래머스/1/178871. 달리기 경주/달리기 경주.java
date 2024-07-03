0e72368036d178e643dbcdd4cbce9a771ad85384
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2024-07-03 11:20:20 +0900

    [level 1] Title: 달리기 경주, Time: 0.07 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -4,29 +4,24 @@ class Solution {
        // 플레이어들의 이름과 인덱스를 저장하는 HashMap
         HashMap<String, Integer> map = new HashMap<>();
         
-        // 초기화: 플레이어 이름과 인덱스 매핑
+        
         for (int i = 0; i < players.length; i++) {
             map.put(players[i], i);
         }
         
-        // 호출 목록을 처리
-        for (String calling : callings) {
-            // 호출된 선수의 인덱스
-            int currentIndex = map.get(calling);
+        
+        for(int i=0; i< callings.length; i++){
+            int index = map.get(callings[i]); //3이겠지? kai의 index는 3임
             
-            // 만약 첫 번째 선수가 아니라면 위치를 바꿈
-            if (currentIndex > 0) {
-                // 배열에서 호출된 선수를 한 칸 앞으로 이동
-                String temp = players[currentIndex];
-                players[currentIndex] = players[currentIndex - 1];
-                players[currentIndex - 1] = temp;
-                
-                // HashMap에서 위치 업데이트
-                map.put(players[currentIndex], currentIndex);
-                map.put(players[currentIndex - 1], currentIndex - 1);
-            }
+            String str = players[index-1]; 
+            players[index-1] = callings[i];
+            players[index] = str;
+            map.put(callings[i], index-1);
+            map.put(str, map.get(str)+1);
+            
         }
         
+       
         // 결과 배열 반환
         return players;
     }
