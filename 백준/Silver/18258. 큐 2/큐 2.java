59027d4ef4f928b783b68caef7277bc91595931c
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2025-01-20 22:44:48 +0900

    [Silver IV] Title: 큐 2, Time: 1180 ms, Memory: 323064 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,46 +1,47 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int n = Integer.parseInt(br.readLine());
-        int[] queue = new int[n];
-        int front = 0;
-        int back = 0;
-
-        for (int i = 0; i < n; i++) {
+        int a = Integer.parseInt(br.readLine());
+        int[] arr = new int[a];
+        int frontcheck = 0;
+        int backcheck = 0;
+        for(int i=0; i<a; i++){
             String command = br.readLine();
-            if (command.startsWith("push")) {
-                int value = Integer.parseInt(command.split(" ")[1]);
-                queue[back++] = value;
-            } else if (command.equals("front")) {
-                if (front == back) {
-                    bw.write("-1\n");
-                } else {
-                    bw.write(queue[front] + "\n");
+            if(command.length()>=6){
+                int pushNum = Integer.parseInt(command.split(" ")[1]);
+                arr[backcheck++] = pushNum;
+            }else if(command.equals("front")){
+                if(frontcheck == backcheck){
+                    bw.write(-1+"\n");
+                }else{
+                    bw.write(arr[frontcheck]+"\n");
                 }
-            } else if (command.equals("pop")) {
-                if (front == back) {
-                    bw.write("-1\n");
-                } else {
-                    bw.write(queue[front++] + "\n");
+            }else if(command.equals("pop")){
+                if(frontcheck==backcheck){
+                    bw.write(-1+"\n");
+                }else{
+                    bw.write(arr[frontcheck++]+"\n");
                 }
-            } else if (command.equals("size")) {
-                bw.write((back - front) + "\n");
-            } else if (command.equals("empty")) {
-                bw.write((front == back ? 1 : 0) + "\n");
+            }else if(command.equals("size")){
+                bw.write(backcheck-frontcheck+"\n");
+            }else if(command.equals("empty")){
+                if(frontcheck == backcheck){
+                    bw.write(1+"\n");
+                }else{
+                    bw.write(0+"\n");
+                }
             } else if (command.equals("back")) {
-                if (front == back) {
+                if (frontcheck == backcheck) {
                     bw.write("-1\n");
                 } else {
-                    bw.write(queue[back - 1] + "\n");
+                    bw.write(arr[backcheck - 1] + "\n");
                 }
             }
-        }
 
+        }
         br.close();
         bw.flush();
         bw.close();
