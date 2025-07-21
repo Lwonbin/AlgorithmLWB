174a6a46cb0cc761535dbebcf2b9cd8d74fbf362
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2025-07-21 23:36:00 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 448 ms, Memory: 91328 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
@@ -3,53 +3,56 @@ import java.util.*;
 
 
 public class Main {
+
     static int[] arr;
-    static int N, K;
+    static int N,K;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
-
-
-
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        arr = new int[N];
-
         st = new StringTokenizer(br.readLine());
 
-        for(int i = 0; i<N; i++){
+        arr = new int[N];
+
+        for(int i=0; i<N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
 
-        int start =0;
+
+
+        int start = 0;
         int end = 0;
-        int result = Integer.MIN_VALUE;
 
         int check = 0;
 
-        while(end < N){
+        int result = 0;
+
+
+//        12345678
+        while(end<N){
             if(arr[end] % 2 != 0){
                 check++;
             }
-
+            
             while(check > K){
-                if(arr[start] % 2 == 1) check--;
+                if(arr[start] % 2 != 0){
+                    check--;
+                }
                 start++;
             }
-
-
-
-            result = Math.max(result, end - start +1 - check);
+            
+            result = Math.max(result, end-start+1 -check);
             end++;
-
         }
+
 
         System.out.println(result);
 
