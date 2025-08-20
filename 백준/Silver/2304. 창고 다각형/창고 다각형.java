b658fcecd5e19c3f4cf917e2c6d240801fdb7500
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2025-08-20 16:03:09 +0900

    [Silver I] Title: 창고 다각형, Time: 116 ms, Memory: 14744 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -1,66 +1,74 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[1001];
-        int start = Integer.MAX_VALUE;
-        int end = 0;
 
-        Stack<Integer> height = new Stack<>();
+        int[][] arr = new int[N][2];
 
+        int max = 0;
 
         for(int i=0; i<N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int L = Integer.parseInt(st.nextToken());
-            int H = Integer.parseInt(st.nextToken());
-
-            arr[L] = H;
-            start = Math.min(L, start);
-            end = Math.max(L, end);
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            max = Math.max(max, y);
+            arr[i][0] = x;
+            arr[i][1] = y;
         }
 
-        int temp = arr[start];
-        for(int i= start +1; i<= end; i++){
-            if(arr[i]<temp){
-                height.push(i);
-            }else{
-                while(!height.isEmpty()){
-                    int x = height.pop();
-                    arr[x] = temp;
-                }
-                temp=arr[i];
-            }
-        }
-        height.clear();
+
+        Arrays.sort(arr, Comparator.comparingInt(r -> r[0]));
 
 
+        int start = 0;
+        int end = N-1;
 
-        temp=arr[end];
-        for(int i= end-1; i>=start; i--){
-            if(arr[i]<temp) height.push(i);
-            else{
-                while(!height.isEmpty()){
-                    int x = height.pop();
-                    arr[x] = temp;
-                }
-                temp = arr[i];
+
+        while(arr[start][1] != max) start++;
+        while(arr[end][1] != max) end--;
+
+
+        int curX = arr[0][0];
+        int curY = arr[0][1];
+
+        int answer =0;
+        for(int i=1; i<=start; i++){
+            if(curY <= arr[i][1]){
+                answer += (arr[i][0] - curX) * curY;
+                curX = arr[i][0];
+                curY = arr[i][1];
             }
         }
 
-        int result = 0;
-        for(int i= start; i<=end; i++){
-            result += arr[i];
+
+
+        curX = arr[N-1][0];
+        curY = arr[N-1][1];
+
+        for(int i = N-2; i>=end; i--){
+            if(curY <= arr[i][1]){
+                answer += (curX - arr[i][0]) * curY;
+                curX = arr[i][0];
+                curY = arr[i][1];
+            }
         }
 
-        System.out.println(result);
 
+        answer += (arr[end][0] - arr[start][0] +1) * max;
+
+
+        System.out.println(answer);
 
         br.close();
-        bw.flush();
-        bw.close();
+
+
     }
 }
