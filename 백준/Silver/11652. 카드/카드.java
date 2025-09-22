e20989ea4037548be9be7abd4d832aa3dfed2934
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2025-09-22 18:21:19 +0900

    [Silver IV] Title: 카드, Time: 372 ms, Memory: 34156 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        long N = Long.parseLong(br.readLine());
+        HashMap<Long, Integer> map = new HashMap<>();
+
+        for(int i=0; i<N; i++){
+            long cur = Long.parseLong(br.readLine());
+            map.put(cur, map.getOrDefault(cur, 0)+1);
+        }
+
+        int max = Integer.MIN_VALUE;
+        long result = 0;
+
+        for(Map.Entry<Long, Integer> entry: map.entrySet()){
+            if(entry.getValue() > max){
+                max = entry.getValue();
+                result = entry.getKey();
+            }else if(entry.getValue() == max){
+                result = Math.min(result, entry.getKey());
+            }
+        }
+
+        System.out.println(result);
+
+
+
+        br.close();
+
+    }
+}
