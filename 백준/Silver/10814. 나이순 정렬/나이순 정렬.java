8a0772d5abfc7b9ae021d41fe5a86f0083ea295b
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2025-10-23 16:33:44 +0900

    [Silver V] Title: 나이순 정렬, Time: 844 ms, Memory: 55200 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,57 +1,63 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-
-        List<Person> list = new ArrayList<>();
-
-        for(int i=0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int age = Integer.parseInt(st.nextToken());
-            String name = st.nextToken();
-            int index = i;
-            Person person1 = new Person(age, name, index);
-            list.add(person1);
-        }
-
-        list.sort((o1,o2) -> {
-           int cmp = o1.age - o2.age;
-           if(cmp==0){
-               return o1.index - o2.index;
-           }
-           return cmp;
-        });
-
-
-        for(int i=0; i<list.size(); i++){
-            System.out.println(list.get(i).age +" " +list.get(i).name);
-        }
-
-
-        br.close();
-
-
-    }
-
 
     static class Person{
         int age;
         String name;
 
-        int index;
-
-        Person(int age, String name, int index){
+        Person(int age, String name){
             this.age = age;
             this.name = name;
-            this.index = index;
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        HashMap<Person, Integer> map = new HashMap<>();
+
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+
+            Person person = new Person(age, name);
+            map.put(person, i);
+
+
+        }
+
+
+        List<Person> List = new ArrayList<>(map.keySet());
+
+        List.sort((o1, o2) -> {
+            if(o1.age == o2.age){
+                return map.get(o1) - map.get(o2);
+            }
+
+            return o1.age - o2.age;
+        });
+
+        for(int i=0; i<N; i++){
+            Person person = List.get(i);
+            bw.write(person.age +" "+person.name);
+            bw.write("\n");
+        }
+
+
+
+
+
+        br.close();
+        bw.flush();
+        bw.close();
 
     }
 }
