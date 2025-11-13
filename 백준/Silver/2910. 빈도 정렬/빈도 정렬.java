8c8d80e60aae3e75d53e88af0bada0c2502789cb
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2025-11-13 17:59:07 +0900

    [Silver III] Title: 빈도 정렬, Time: 128 ms, Memory: 14976 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -6,52 +6,46 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-       StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-       int N = Integer.parseInt(st.nextToken());
-       int C = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
 
-       st= new StringTokenizer(br.readLine());
-       HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map2 = new HashMap<>();
 
-       HashMap<Integer, Integer> map2 = new HashMap<>();
+        st = new StringTokenizer(br.readLine());
 
+        int count = st.countTokens();
 
+        for (int i = 0; i < count; i++) {
+            int cur = Integer.parseInt(st.nextToken());
 
+            if(!map2.containsKey(cur)){
+                map2.put(cur, i);
+            }
+            map.put(cur, map.getOrDefault(cur,0)+1);
+        }
 
-       for(int i =0; i<N; i++){
-           int cur = Integer.parseInt(st.nextToken());
-           map.put(cur, map.getOrDefault(cur, 0)+1);
-           if(!map2.containsKey(cur)){
-               map2.put(cur, i);
-           }
+        List<Integer> list = new ArrayList<>(map.keySet());
 
+        list.sort((o1, o2) ->{
+            if(map.get(o1) == map.get(o2)){
+                return map2.get(o1) - map2.get(o2);
+            }
 
-       }
+            return map.get(o2) - map.get(o1);
+        });
 
-       List<Integer> list = new ArrayList<>(map.keySet());
-
-       list.sort((o1,o2) -> {
-           int cmp = map.get(o2).compareTo(map.get(o1));
-           if(cmp == 0){
-               return map2.get(o1).compareTo(map2.get(o2));
-           }
-           return cmp;
-
-       });
-
-
-       for(int i=0; i<list.size(); i++){
-           for(int j=0; j<map.get(list.get(i)); j++){
-               System.out.print(list.get(i)+" ");
-           }
-       }
-
-
+        for(int i=0; i<map.size(); i++){
+            for(int j=0; j<map.get(list.get(i)); j++){
+                bw.write(list.get(i)+" ");
+            }
+        }
 
         br.close();
         bw.flush();
         bw.close();
-
     }
+
 }
