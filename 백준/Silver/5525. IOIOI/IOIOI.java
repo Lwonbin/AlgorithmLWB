fac6cdd6b95f5d3f57161fdf5e78e3e34a99dbdf
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2025-11-25 23:49:35 +0900

    [Silver I] Title: IOIOI, Time: 244 ms, Memory: 20192 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,44 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int N = Integer.parseInt(br.readLine());
+
+        int M = Integer.parseInt(br.readLine());
+
+        String S = br.readLine();
+
+        int result = 0;
+        int cnt = 0;
+
+        for(int i=1; i<M-1; i++){
+            if(S.charAt(i-1) == 'I' && S.charAt(i) == 'O' && S.charAt(i+1) == 'I'){
+                cnt++;
+                if(cnt == N){
+                    result++;
+                    cnt--;
+                }
+                i++;
+            }else{
+                cnt = 0;
+            }
+        }
+
+        System.out.println(result);
+
+
+
+
+        br.close();
+
+    }
+}
