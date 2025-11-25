857b5047fe6dc32704b2844a33cf133419363a74
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2025-11-25 11:45:01 +0900

    [Gold V] Title: 토마토, Time: 568 ms, Memory: 121624 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,48 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[][] graph;
-    static int[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static int target  =0;
-    static Queue<int[]> queue =  new LinkedList<>();
+
+
+    static int N,M;
+    static int[][] map;
+    static boolean[][] visited;
+
+
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+
+    static Queue<int[]> queue = new LinkedList<>();
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        graph = new int[N][M];
-        visited = new int[N][M];
+
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
 
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<M; j++){
-                graph[i][j] = Integer.parseInt(st.nextToken());
-                if(graph[i][j] == 1){
-                    queue.add(new int[] {i,j});
+                int tomato = Integer.parseInt(st.nextToken());
+                map[i][j] = tomato;
+                if(tomato == 1){
+                    queue.add(new int[]{i,j});
                 }
             }
         }
+
 
         bfs();
 
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (graph[i][j] == 0) {
-                    bw.write("-1");
-                    bw.flush();
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(map[i][j] == 0){
+                    System.out.println(-1);
                     return;
                 }
-                target = Math.max(target, visited[i][j]);
+
+                max = Math.max(max, map[i][j]);
             }
         }
-        bw.write(target + "");
+
+        System.out.println(max-1);
+
+
 
 
 
@@ -55,28 +70,25 @@ public class Main {
     }
 
     static void bfs(){
+
         while(!queue.isEmpty()){
             int[] cur = queue.poll();
-            int r =  cur[0];
-            int c = cur[1];
+            int curX = cur[0];
+            int curY = cur[1];
 
+            for(int i =0; i<4; i++){
+                int nx = curX +dx[i];
+                int ny = curY + dy[i];
 
-            for(int i=0; i<4; i++){
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
-
-                if(nx>=0 && ny>=0 && nx<N && ny<M){
-                    if(graph[nx][ny] == 0 && visited[nx][ny] == 0){
+                if(nx>=0&& ny>=0 && nx<N && ny<M){
+                    if(!visited[nx][ny] && map[nx][ny] == 0){
+                        visited[nx][ny] = true;
+                        map[nx][ny] = map[curX][curY] + 1;
                         queue.add(new int[]{nx, ny});
-                        visited[nx][ny] = visited[r][c] +1;
-                        graph[nx][ny] = 1;
                     }
                 }
             }
         }
-
-
-
 
     }
 }
