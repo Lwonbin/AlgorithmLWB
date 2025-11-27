102470d6bde13482042642f196aeae656118100c
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2025-11-28 00:06:13 +0900

    [level 3] Title: 순위, Time: 13.77 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -4,61 +4,54 @@ class Solution {
     public int solution(int n, int[][] results) {
         int answer = 0;
         
-        int[] ranking = new int[n+1];
-        
         List<Integer>[] win = new ArrayList[n+1];
         List<Integer>[] lose = new ArrayList[n+1];
         
-        for(int i =0; i<n+1; i++){
+        for(int i=1; i<=n; i++){
             win[i] = new ArrayList<>();
             lose[i] = new ArrayList<>();
         }
         
-        for(int i=0; i<results.length; i++){
-            int u = results[i][0];
-            int v = results[i][1];
+        for(int i =0; i<results.length; i++){
+            int a = results[i][0];
+            int b = results[i][1];
             
-            win[u].add(v);
-            lose[v].add(u);
+            win[a].add(b);
+            lose[b].add(a);
         }
         
-        
-        for(int i=1; i<n+1; i++){
+        for(int i=1; i<=n; i++){
             boolean[] visited = new boolean[n+1];
-            int wins = bfs(i, win, visited);
-            int loses = bfs(i, lose, visited);
+            int winCNT = bfs(i, visited, win);
+            int loseCNT = bfs(i, visited, lose);
             
-            if(wins + loses == n-1){
+            if(winCNT + loseCNT == n-1){
                 answer++;
             }
         }
         
-        
-        
-        
         return answer;
     }
     
-    
-    static int bfs(int start, List<Integer>[] graph, boolean[] visited){
+    static int bfs(int i, boolean[] visited, List<Integer>[] list){
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        visited[start] = true;
+        queue.add(i);
+        visited[i] = true;
+        
         int count = 0;
         
         while(!queue.isEmpty()){
             int cur = queue.poll();
             
-            for(int next : graph[cur]){
+            for(int next : list[cur]){
                 if(!visited[next]){
                     count++;
-                    visited[next] = true;
                     queue.add(next);
+                    visited[next] = true;
                 }
             }
         }
         
         return count;
-        
     }
 }
