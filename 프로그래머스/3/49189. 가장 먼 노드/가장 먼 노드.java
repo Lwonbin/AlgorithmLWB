fcadc6f24294257c9c7f952ba1e6c11468d86711
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2025-11-27 23:06:33 +0900

    [level 3] Title: 가장 먼 노드, Time: 24.91 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,52 +1,51 @@
 import java.util.*;
 
 class Solution {
+    
     public int solution(int n, int[][] edge) {
+        
         int answer = 0;
-        List<Integer>[] graph = new ArrayList[n+1];
         
-        int[] dist = new int[n+1];
-        Arrays.fill(dist, -1);
-        
-        
-        for(int i=0; i<n+1; i++){
-            graph[i] = new ArrayList<>();
+        List<Integer>[] list = new ArrayList[n+1];
+        for(int i=1; i<n+1; i++){
+            list[i] = new ArrayList<>();
         }
-        
         
         
         for(int i=0; i<edge.length; i++){
-            int start = edge[i][0];
-            int end = edge[i][1];
+            int from = edge[i][0];
+            int to = edge[i][1];
             
-            graph[start].add(end);
-            graph[end].add(start);
+            list[from].add(to);
+            list[to].add(from);
         }
         
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(1);
+        int[] dist = new int[n+1];
+        Arrays.fill(dist, -1);
         dist[1] = 0;
+        
+        Queue<Integer> queue = new LinkedList<>();
+        
+        queue.add(1);
+        
+        int max = Integer.MIN_VALUE;
         
         
         while(!queue.isEmpty()){
             int cur = queue.poll();
             
-            for(int next : graph[cur]){
+            for(int next : list[cur]){
                 if(dist[next] == -1){
                     dist[next] = dist[cur] +1;
                     queue.add(next);
+                    max = dist[next];
                 }
+                
             }
         }
         
-        int max = 0;
-        for(int i=0; i<n+1; i++){
-            max = Math.max(dist[i], max);
-            System.out.print(dist[i]+" ");
-        }
-        
-        for(int i=0; i<n+1; i++){
-            if(max == dist[i]){
+        for(int i=1; i<n+1; i++){
+            if(dist[i] == max){
                 answer++;
             }
         }
