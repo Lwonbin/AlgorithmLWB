a0f081a0857a8c774db9576b57799024d8b00601
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2025-11-28 22:41:40 +0900

    [Silver V] Title: 나이순 정렬, Time: 672 ms, Memory: 50896 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -3,61 +3,58 @@ import java.util.*;
 
 public class Main {
 
-    static class Person{
+    static class Person {
         int age;
         String name;
+        int index;
 
-        Person(int age, String name){
+        Person(int age, String name, int index) {
             this.age = age;
             this.name = name;
+            this.index = index;
         }
+
+
     }
 
     public static void main(String[] args) throws IOException {
 
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+
         int N = Integer.parseInt(br.readLine());
 
-        HashMap<Person, Integer> map = new HashMap<>();
+        List<Person> list = new ArrayList<>();
 
         for(int i=0; i<N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int age = Integer.parseInt(st.nextToken());
             String name = st.nextToken();
 
-            Person person = new Person(age, name);
-            map.put(person, i);
-
-
+            Person person = new Person(age, name, i);
+            list.add(person);
         }
 
 
-        List<Person> List = new ArrayList<>(map.keySet());
+        list.sort((o1, o2) ->{
+           if(o1.age == o2.age){
+               return o1.index - o2.index;
+           }
 
-        List.sort((o1, o2) -> {
-            if(o1.age == o2.age){
-                return map.get(o1) - map.get(o2);
-            }
-
-            return o1.age - o2.age;
+           return o1.age - o2.age;
         });
 
         for(int i=0; i<N; i++){
-            Person person = List.get(i);
-            bw.write(person.age +" "+person.name);
+            bw.write(list.get(i).age+" "+list.get(i).name);
             bw.write("\n");
         }
 
 
-
-
-
-        br.close();
         bw.flush();
         bw.close();
+        br.close();
+
 
     }
 }
