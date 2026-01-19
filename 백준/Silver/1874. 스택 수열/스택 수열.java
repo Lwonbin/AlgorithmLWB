977a6312b57adb7b445ac50aaca9cceab6757935
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-01-19 18:58:13 +0900

    [Silver II] Title: 스택 수열, Time: 296 ms, Memory: 26380 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -5,42 +5,41 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
+
         StringBuilder sb = new StringBuilder();
 
-        Stack<Integer> intStack = new Stack<>();
-        int start = 0;
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
 
-        for(int i=0; i<N; i++){
-            int n = Integer.parseInt(br.readLine());
-            if(n > start){
-                for(int j=start+1; j<=n;j++){
-                    intStack.push(j);
-                    sb.append("+").append("\n");
+        int cur = 0;
+
+        for(int i = 0; i < n; i++){
+            int N = Integer.parseInt(br.readLine());
+
+            if(N>cur){
+                for(int j = cur+1; j <= N; j++) {
+                    stack.push(j);
+                    sb.append("+\n");
                 }
-                start = n;
-            }else if(intStack.peek() != n){
+                cur = N;
+            }else if(!stack.isEmpty() && stack.peek() != N){
                 System.out.println("NO");
                 return;
+
             }
 
-            intStack.pop();
-            sb.append("-").append("\n");
+            stack.pop();
+            sb.append("-\n");
+
         }
+
+
+
         bw.write(String.valueOf(sb));
-
-
-
 
 
         br.close();
         bw.flush();
         bw.close();
-
-
-
-
-
-
     }
 }
