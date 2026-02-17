84729f431dbf20d9f494c9747e1e74afeb749939
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-02-17 22:57:37 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT A.ANIMAL_ID, A.ANIMAL_TYPE, A.NAME from ANIMAL_INS A join ANIMAL_OUTS B on A.ANIMAL_ID = B.ANIMAL_ID
+
+WHERE (A.SEX_UPON_INTAKE = "Intact Male" AND B.SEX_UPON_OUTCOME = "Neutered Male")
+OR (A.SEX_UPON_INTAKE = "Intact Female" AND B.SEX_UPON_OUTCOME = "Spayed Female")
+;
