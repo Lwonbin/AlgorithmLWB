51f764f2e8e92a1ae1965054b57cd6604b488f8c
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-02-17 12:38:39 +0900

    [Gold V] Title: AC, Time: 544 ms, Memory: 98872 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,81 +1,106 @@
-import java.io.*;
-import java.util.*;
+    import java.io.*;
+    import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public class Main {
 
-        int N = Integer.parseInt(br.readLine());
+        public static void main(String[] args) throws IOException {
 
-        for(int i=0; i<N; i++){
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-            String def = br.readLine();
+            int N = Integer.parseInt(br.readLine());
 
-            int arrN = Integer.parseInt(br.readLine());
-            Deque<String> deque = new ArrayDeque<>();
-            String arr = br.readLine();
-            String subArr = arr.substring(1,arr.length()-1);
-            String[] ARR = subArr.split(",");
+            for(int i=0; i<N; i++){
+                StringBuilder sb = new StringBuilder();
+                String p = br.readLine();
+                int n = Integer.parseInt(br.readLine());
+
+                String cur = br.readLine();
+                String newCur = cur.substring(1, cur.length()-1);
 
 
-            for(int j=0; j<ARR.length; j++){
-                if(!ARR[j].equals("R") && !ARR[j].equals("D") && !ARR[j].isEmpty()){
-                    deque.addLast(ARR[j]);
-                }
-            }
+                String[] arr = newCur.split(",");
 
-            int curDist = 1;
-            boolean check = true;
+                Deque<Integer> queue = new ArrayDeque<>();
 
-            for(int j=0; j<def.length(); j++){
-                if(def.charAt(j)=='R'){
-                    curDist = -curDist;
-                }else if(def.charAt(j)=='D'){
-                    if(deque.isEmpty()){
-                        bw.write("error"+"\n");
-                        check = false;
-                        break;
+                boolean check0 = true;
+                if(n == 0){
+                    for(int j=0; j<p.length(); j++){
+                        if(p.charAt(j)=='D'){
+                            check0 = false;
+                        }
                     }
-
-                    if(curDist == 1){
-                        deque.pollFirst();
+                    if(!check0){
+                        System.out.println("error");
+                        continue;
                     }else{
-                        deque.pollLast();
+                        System.out.println("[]");
+                        continue;
                     }
-                }
-            }
-            if(check){
-                if(curDist==1){
-                    bw.write("[");
-                    while(!deque.isEmpty()){
-                        if(deque.size()==1){
-                            bw.write(deque.pollFirst());
-                            break;
-                        }
-                        bw.write(deque.pollFirst() +",");
-                    }
-                    bw.write("]");
-                }else{
-                    bw.write("[");
-                    while(!deque.isEmpty()){
-                        if(deque.size()==1){
-                            bw.write(deque.pollLast());
-                            break;
-                        }
-                        bw.write(deque.pollLast() +",");
-                    }
-                    bw.write("]");
                 }
 
-                bw.write("\n");
+
+
+                for(int j=0; j<arr.length; j++){
+//                    System.out.print(arr[j]);
+                    queue.add(Integer.parseInt(arr[j]));
+                }
+
+                int desc = 1;
+                boolean check = true;
+
+                for(int j = 0; j<p.length(); j++){
+
+                    char curP = p.charAt(j);
+                    if(curP == 'R'){
+                        desc *= -1;
+                    }else{
+                        if(queue.isEmpty()){
+                            check = false;
+                            break;
+                        }
+                        else if(desc == 1){
+                            queue.pollFirst();
+                        }else if(desc == -1){
+                            queue.pollLast();
+                        }
+                    }
+                }
+
+
+                if(desc == 1){
+                    sb.append("[");
+                    int size = queue.size();
+                    for(int j=0; j<size; j++){
+                        if(j == size-1){
+                            sb.append(queue.pollFirst());
+                        }else{
+                            sb.append(queue.pollFirst()).append(",");
+                        }
+                    }
+                    sb.append("]");
+                }else if(desc == -1){
+                    sb.append("[");
+                    int size = queue.size();
+                    for(int j=0; j<size; j++){
+                        if(j == size-1){
+                            sb.append(queue.pollLast());
+                        }else{
+                            sb.append(queue.pollLast()).append(",");
+                        }
+                    }
+                    sb.append("]");
+                }
+                if(check){
+                    System.out.println(sb.toString());
+                }else{
+                    System.out.println("error");
+                }
+
             }
+
+
+
 
         }
-
-
-        br.close();
-        bw.flush();
-        bw.close();
     }
-}
