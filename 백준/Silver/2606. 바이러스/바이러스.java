40dc8c4ded30eebb9570c6fe24ab17e215e40dca
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-02-19 17:01:12 +0900

    [Silver III] Title: 바이러스, Time: 96 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,48 +1,54 @@
-import java.io.*;
-import java.util.*;
-public class Main {
-    static int count, num, connections;
-    static boolean[] visited;
-    static List[] computers;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    import java.io.*;
+    import java.util.*;
 
-        num = Integer.parseInt(br.readLine());
-        connections = Integer.parseInt(br.readLine());
+    public class Main {
 
-        visited = new boolean[num+1];
-        computers = new List[num+1];
-        count = 0;
 
-        for(int i=1; i<num+1; i++){
-            computers[i] = new ArrayList<Integer>();
+        public static void main(String[] args) throws IOException {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+           int N = Integer.parseInt(br.readLine());
+
+           List<Integer>[] computer = new List[N+1];
+
+           for(int i=0; i<=N; i++){
+               computer[i] = new ArrayList<>();
+           }
+
+           int M = Integer.parseInt(br.readLine());
+           for(int i=0; i<M; i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int A = Integer.parseInt(st.nextToken());
+                int B = Integer.parseInt(st.nextToken());
+
+                computer[A].add(B);
+                computer[B].add(A);
+           }
+
+
+            boolean[] visited = new boolean[N+1];
+           Queue<Integer> queue = new ArrayDeque<>();
+           queue.add(1);
+
+           int answer = 0;
+
+           visited[1] = true;
+
+           while(!queue.isEmpty()){
+               int cur = queue.poll();
+
+               for(int nxt : computer[cur]){
+                   if(!visited[nxt]){
+                       queue.add(nxt);
+                       visited[nxt] = true;
+                       answer++;
+                   }
+               }
+           }
+
+            System.out.println(answer);
         }
-
-        StringTokenizer st;
-        for(int i=0; i<connections; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            computers[a].add(b);
-            computers[b].add(a);
-        }
-
-        dfs(1);
-
-        System.out.println(count-1);
-        br.close();
-
     }
-
-
-    private static void dfs(int start){
-        if(!visited[start]){
-            visited[start] = true;
-            count++;
-            for(int i=0; i< computers[start].size(); i++){
-                dfs((Integer) computers[start].get(i));
-            }
-        }
-    }
-}
