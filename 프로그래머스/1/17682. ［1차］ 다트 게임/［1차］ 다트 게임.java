8cb9fedf663f3c2cde17e6930fdb4371a5919d5f
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-02-20 15:44:44 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.27 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -2,47 +2,58 @@ class Solution {
     public int solution(String dartResult) {
         int answer = 0;
         
-        int[] dart = new int[3];
         
-        String numstr = "";
+        int[] score = new int[3];
+        
+        String[] strArr = dartResult.split("D|S|T");
+        
+        for(int i=0; i<3; i++){
+            String cur = strArr[i];
+            StringBuilder sb = new StringBuilder();
+            for(int j=0; j<cur.length(); j++){
+                if(cur.charAt(j)!='*' && cur.charAt(j)!='#'){
+                    sb.append(cur.charAt(j));
+                }
+            }
+            score[i] = Integer.parseInt(sb.toString());
+        }
+        
+        
+        
+        
+        
         int idx = 0;
-        int num = 0;
-        
         for(int i=0; i<dartResult.length(); i++){
-            char ch = dartResult.charAt(i);
-            
-            
-            if(ch>='0' && ch<='9'){
-                numstr += String.valueOf(ch);
- 
+            int sum = 0;
+            if(dartResult.charAt(i) == 'S'){
+                idx++;
                 
-            }
-            else if(ch =='S' || ch == 'D' || ch == 'T'){
-                num = Integer.parseInt(numstr);
-                if(ch == 'S'){
-                    dart[idx++] = (int)Math.pow(num,1);
-                }else if(ch == 'D'){
-                    dart[idx++] = (int)Math.pow(num,2);
+            }else if(dartResult.charAt(i) == 'D'){
+                score[idx] *= score[idx];
+                idx++;
+            
+            }else if(dartResult.charAt(i) == 'T'){
+                score[idx] *= score[idx] * score[idx];
+                idx++;
+                
+            }else if(dartResult.charAt(i) == '*'){
+                if(idx == 1){
+                    score[0] = score[0] * 2;
                 }else{
-                    dart[idx++] = (int)Math.pow(num,3);
+                    for(int j=idx-2; j<idx; j++){
+                        score[j] = score[j]*2;
+                    }
                 }
-                numstr="";
-                    
-            }else{
-                if(ch == '*'){
-                    dart[idx-1] *= 2;
-                    if(idx-2>=0) dart[idx-2] *=2;
-                }
-                else{
-                    dart[idx-1] *= -1;
-                }
+            }else if(dartResult.charAt(i) == '#'){
+                score[idx-1] *= -1;
             }
             
-            
-            answer = dart[0] + dart[1] + dart[2];
-            
+
             
         }
+             for(int i=0; i<3; i++){
+                answer += score[i];
+            }
         
         return answer;
     }
