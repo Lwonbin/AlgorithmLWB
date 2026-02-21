00c6ae3d4dba66045c4440afce2c0cdd7f6d197b
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-02-21 12:49:42 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,7 +1,10 @@
-select h.hour as HOUR, count(b.DATETIME) as COUNT from(
+select h.HOUR AS HOUR, COUNT(ANIMAL_ID) as COUNT from 
 
-select 0 hour union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9 union all select 10 union all select 11 union all select 12 union all select 13 union all select 14 union all select 15 union all select 16 union all select 17 union all select 18 union all select 19 union all select 20 union all select 21 union all select 22 union all select 23) h left join ANIMAL_OUTS B on h.hour = HOUR(b.DATETIME)
+(select 0 hour UNION ALL select 1 UNION ALL select 2 UNION ALL select 3 UNION ALL select 4 UNION ALL select 5 UNION ALL select 6 UNION ALL select 7 UNION ALL select 8 UNION ALL select 9 UNION ALL select 10 UNION ALL select 11 UNION ALL select 12 UNION ALL select 13 UNION ALL select 14 UNION ALL select 15 UNION ALL select 16 UNION ALL select 17 UNION ALL select 18 UNION ALL select 19 UNION ALL select 20 UNION ALL select 21 UNION ALL select 22 UNION ALL select 23) h
+left join ANIMAL_OUTS B on h.hour = HOUR(B.DATETIME)
 
+GROUP BY HOUR
 
-group by HOUR
-order by HOUR;
+order by HOUR
+
+; 
