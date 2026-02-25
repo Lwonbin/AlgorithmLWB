903ec89cc13808801d96b07dc434007cad1d0265
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-02-25 17:55:26 +0900

    [Silver II] Title: 주식, Time: 1040 ms, Memory: 315324 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -1,38 +1,45 @@
-import java.io.*;
-import java.util.StringTokenizer;
+    import java.io.*;
+    import java.sql.SQLOutput;
+    import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public class Main {
+
+        public static void main(String[] args) throws IOException {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
-        int T = Integer.parseInt(br.readLine());
+            int T = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<T; i++){
-            int N = Integer.parseInt(br.readLine());
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            StringBuilder sb= new StringBuilder();
+            for(int i=0; i<T; i++){
+                int N = Integer.parseInt(br.readLine());
 
-            int[] arr = new int[N];
-            for(int j=0; j<N; j++){
-                arr[j] = Integer.parseInt(st.nextToken());
-            }
-            long sum = 0;
-            long max = arr[N-1];
-            for(int p=N-2; p>=0; p--){
-                if(max > arr[p]){
-                    sum += max-arr[p];
-                }else{
-                    max = arr[p];
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int[] arr = new int[N];
+                for(int j=0; j<N; j++){
+                    arr[j] = Integer.parseInt(st.nextToken());
                 }
+
+                long sum = 0;
+
+                int max= Integer.MIN_VALUE;
+                for(int j=N-1; j>=0; j--){
+                    if(max <= arr[j]){
+                        max = arr[j];
+                    }else{
+                        sum += max-arr[j];
+                    }
+                }
+
+                sb.append(sum).append("\n");
+
+
             }
-            bw.write(sum+"\n");
+
+            System.out.println(sb.toString());
+
         }
 
-
-        br.close();
-        bw.flush();
-        bw.close();
-
     }
-}
