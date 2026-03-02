4ac6f28c0efcdf84b440f058a66603b6746658dd
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-03-02 12:12:49 +0900

    [Gold IV] Title: 수 묶기, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -9,88 +9,78 @@
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
-
             int N = Integer.parseInt(br.readLine());
 
-            int checkMinus = 0;
-            int checkPlus = 0;
-            int checkZero = 0;
-            int checkOne = 0;
-            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            int minusCnt = 0;
+            int plusCnt = 0;
+            int zeroCnt = 0;
+            int oneCnt = 0;
 
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+            int[] arr = new int[N];
             for(int i=0; i<N; i++){
                 int cur = Integer.parseInt(br.readLine());
+                if(cur < 0){
+                    minusCnt++;
+                    pq.add(cur);
+                }else if(cur == 1){
+                    oneCnt++;
+                }else if(cur >0){
+                    plusCnt++;
+                    pq.add(cur);
+                }
+                else{
+                    zeroCnt++;
+                }
 
-                if(cur == 1){
-                    checkOne++;
-                }
-                else if(cur > 0){
-                    checkPlus++;
-                    pq.add(cur);
-                }else if(cur <0){
-                    checkMinus++;
-                    pq.add(cur);
-                }else{
-                    checkZero++;
-                }
             }
 
 
+            int[] minus = new int[minusCnt];
+            Integer[] plus = new Integer[plusCnt];
 
-            Integer[] minus = new Integer[checkMinus];
-            Integer[] plus = new Integer[checkPlus];
-
-            for(int i=0; i<checkMinus; i++){
+            int idx = 0;
+            for(int i=0; i<minusCnt; i++){
                 minus[i] = pq.poll();
             }
 
-            for(int i=0; i<checkPlus; i++){
+            for(int i=0; i<plusCnt; i++){
                 plus[i] = pq.poll();
             }
-
 
             Arrays.sort(plus, Collections.reverseOrder());
 
 
+            long sum = 0;
 
-            int sum = 0;
-            sum += checkOne;
+            sum += oneCnt;
 
 
-            if(checkMinus % 2 == 0){
-                for(int i=0; i<checkMinus; i+=2){
-                    sum += minus[i] * minus[i+1];
-                }
-            }else if(checkMinus % 2 == 1){
-                if(checkMinus >1){
-                    for(int i=0; i<checkMinus-1; i+=2){
-                        sum += minus[i] * minus[i+1];
-                    }
-                }
+            for(int i=0; i<minusCnt-1; i+=2){
+                sum += minus[i] * minus[i+1];
+            }
 
-                if(checkZero == 0){
-                    sum+= minus[checkMinus-1];
+            if(minusCnt % 2 == 1){
+                if(zeroCnt == 0){
+                    sum += minus[minusCnt-1];
+                }else{
+                    zeroCnt--;
                 }
             }
 
 
-            if(checkPlus == 1){
-                sum+= plus[0];
-            }else if(checkPlus % 2 == 0){
-                for(int i=0; i<checkPlus; i+=2){
-                    sum += plus[i] * plus[i+1];
-                }
-            }else if(checkPlus % 2 == 1){
-                for(int i=0; i<checkPlus-1; i+=2){
-                    sum+= plus[i] * plus[i+1];
-                }
-                sum+=plus[checkPlus-1];
+            for(int i=0; i<plusCnt-1; i+=2){
+                sum += plus[i] * plus[i+1];
             }
 
-
+            if(plusCnt % 2 == 1){
+                sum += plus[plusCnt-1];
+            }
 
 
             System.out.println(sum);
+
 
 
 
