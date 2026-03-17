4995c1aafcdf9b8b21c85e1c56294982ac8c5507
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-03-17 17:04:26 +0900

    [Silver III] Title: N과 M (5), Time: 296 ms, Memory: 28824 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,76 @@
+    import java.io.*;
+    import java.sql.SQLOutput;
+    import java.util.*;
+
+    public class Main {
+
+        static int N,M;
+        static int[] arr;
+
+        static boolean[] visited;
+
+        static int[] num;
+
+        static StringBuilder sb;
+
+        public static void main(String[] args) throws IOException {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            sb = new StringBuilder();
+
+            st = new StringTokenizer(br.readLine());
+
+            num = new int[N];
+            for(int i=0; i<N; i++){
+                num[i] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(num);
+
+
+
+            arr = new int[M];
+            visited = new boolean[N];
+            dfs(0, 0);
+
+
+            System.out.println(sb.toString());
+
+        }
+
+
+        static void dfs(int depth, int start){
+            if(depth == M){
+                for(int i=0; i<M; i++){
+                    sb.append(arr[i]).append(" ");
+                }
+                sb.append("\n");
+                return;
+            }
+
+
+            for(int i=0; i<N; i++){
+                if(visited[i]) continue;
+
+                visited[i] = true;
+                arr[depth] = num[i];
+                dfs(depth +1, i+1);
+                visited[i] = false;
+            }
+
+
+
+
+        }
+
+
+
+    }
