e6823a2c89ba8b09df2b154a5b0ef5a2eaeeb9c9
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-03-17 16:04:40 +0900

    [Silver III] Title: N과 M (1), Time: 1948 ms, Memory: 77180 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,49 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+    import java.io.*;
+    import java.sql.SQLOutput;
+    import java.util.*;
 
-public class Main {
-    static int N;
-    static int M;
-    static int[] arr;
-    static boolean[] check;
-    static StringBuilder sb = new StringBuilder();
+    public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        static int N,M;
+        static int[] arr;
 
+        static boolean[] visited;
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[M+1];
-        check = new boolean[N+1];
+        public static void main(String[] args) throws IOException {
 
-        dfs(0);
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
-        System.out.println(sb);
-    }
 
-    public static void dfs(int depth) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
 
-        if(depth == M){
-            for(int i=0; i<M; i++){
-                sb.append(arr[i]+" ");
-            }
-            sb.append("\n");
-            return;
+            arr = new int[M];
+            visited = new boolean[N];
+            dfs(0);
         }
 
-        for(int i=1; i<=N; i++){
-            if(!check[i]){
-                check[i] = true;
-                arr[depth] = i;
-                dfs(depth +1);
-                check[i] = false;
+
+        static void dfs(int depth){
+
+            if(depth == M){
+                for(int i=0; i<M; i++){
+                    System.out.print(arr[i] +" ");
+                }
+                System.out.println();
+                return;
             }
+
+            for(int i=0; i<N; i++){
+                if(visited[i]) continue;
+
+                visited[i] = true;
+                arr[depth] = i+1;
+                dfs(depth+1);
+                visited[i] = false;
+
+            }
+
+
+
         }
+
+
+
     }
-}
