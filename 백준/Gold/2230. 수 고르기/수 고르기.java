480b3c69ca31154952988f40d90b812e223fce5f
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-03-19 15:48:11 +0900

    [Gold V] Title: 수 고르기, Time: 344 ms, Memory: 27836 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -1,67 +1,54 @@
-import java.io.*;
-import java.util.*;
+    import java.io.*;
+    import java.sql.SQLOutput;
+    import java.util.*;
+
+    public class Main {
+
+        public static void main(String[] args) throws IOException {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
 
 
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            int[] arr = new int[N];
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-
-
-        int[] arr = new int[N];
-
-
-
-        for(int i = 0; i < N; i++){
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-
-
-        int start = 0;
-        int end = 0;
-        int min = Integer.MAX_VALUE;
-
-        Arrays.sort(arr);
-
-
-        while(start <= end){
-            if(end > N-1){
-                break;
-            }
-
-            if(arr[end] - arr[start] >= M){
-                min = Math.min(min, arr[end]-arr[start]);
-                start++;
-            }else if(arr[end] - arr[start] < M){
-                end++;
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(br.readLine());
 
             }
 
+            Arrays.sort(arr);
+
+
+            int start = 0;
+            int end = 1;
+
+            int min = Integer.MAX_VALUE;
+
+            while(start <= end){
+
+                if(end >= N){
+                    break;
+                }
+                int cur = arr[end] - arr[start];
+
+                if(cur < M){
+                    end++;
+                }else{
+                    min = Math.min(min, cur);
+                    start++;
+                }
+            }
+
+            System.out.println(min);
+
+
         }
 
-
-
-
-
-
-        System.out.println(min);
-
-
-
-
-
-
-        br.close();
-        bw.flush();
-        bw.close();
     }
-}
