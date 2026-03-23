15315cc045ea858e5aef13495571b58a62fff8c9
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-03-23 16:14:15 +0900

    [Silver IV] Title: 수들의 합 2, Time: 124 ms, Memory: 15200 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,59 +1,59 @@
-import java.io.*;
-import java.util.StringTokenizer;
+    import java.io.*;
+    import java.sql.SQLOutput;
+    import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public class Main {
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
+        public static void main(String[] args) throws IOException {
 
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(st2.nextToken());
-        }
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int start = 0;
-        int end = 0;
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
 
-        int cnt = 0;
+            int[] arr = new int[N];
+            st = new StringTokenizer(br.readLine());
 
-        int sum=0;
-
-        for(int i = start; i<=end; i++){
-            sum += arr[i];
-        }
-
-        for(int i=0;; i++){
-            try{
-                if(sum == M){
-                  cnt++;
-                  sum += arr[++end] - arr[start++];
-                }else if(sum<M){
-                    sum += arr[++end];
-                }else{
-                    sum -= arr[start];
-                    start++;
-                }
-            } catch (Exception e){
-                break;
+            for(int i = 0;i <N; i++){
+                arr[i] = Integer.parseInt(st.nextToken());
             }
 
+
+            int start = 0;
+            int end = 0;
+
+            int sum = 0;
+
+
+            int answer = 0;
+
+            while(start<=end){
+
+
+                if(sum < M){
+                    if(end >= N){
+                        break;
+                    }
+                    sum+=arr[end++];
+                }else if(sum > M){
+                    sum-=arr[start++];
+                }else{
+                    answer++;
+                    sum-=arr[start++];
+
+                }
+            }
+
+            System.out.println(answer);
+
+
+
+
         }
-        System.out.println(cnt);
-
-
-
-
-        br.close();
-        bw.flush();
-        bw.close();
 
     }
-}
