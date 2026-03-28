ff34178b3cbe857291c5a93145a840c494a0291c
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-03-28 23:02:36 +0900

    [Silver II] Title: 결혼식, Time: 172 ms, Memory: 17312 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.java
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -1,73 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+    import java.io.*;
+    import java.sql.SQLOutput;
+    import java.util.*;
 
-public class Main {
-
-    static List<Integer>[] graph;
-    static boolean[] visited;
-    public static void main(String[] args) throws IOException {
+    public class Main {
 
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        public static void main(String[] args) throws IOException {
 
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        graph = new ArrayList[N+1];
-        visited = new boolean[N+1];
+            int n = Integer.parseInt(br.readLine());
+            int m = Integer.parseInt(br.readLine());
 
-        for(int i=1; i<=N; i++){
-            graph[i] = new ArrayList<>();
-        }
-
-        for(int i=0; i<M; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-
-            graph[u].add(v);
-            graph[v].add(u);
-        }
-
-
-        int count = bfs(1,0);
-
-        System.out.println(count);
-
-
-
-
-        br.close();
-
-    }
-
-    static int bfs(int cur, int count){
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{cur, count});
-        visited[cur] = true;
-        int cnt = 0;
-
-        while(!queue.isEmpty()){
-            int[] friend = queue.poll();
-            int curI = friend[0];
-            int curCount = friend[1];
-
-            if(curCount >= 2){
-                continue;
+            List<Integer>[] list = new List[n+1];
+            for(int i=1; i<=n; i++){
+                list[i] = new ArrayList<>();
             }
 
-            for(int next: graph[curI]){
-                if(!visited[next]){
-                    visited[next] = true;
-                    cnt++;
-                    queue.add(new int[]{next, curCount+1});
+            for(int i=0; i<m; i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
+
+                list[from].add(to);
+                list[to].add(from);
+            }
+
+            int[] check = new int[n+1];
+            Arrays.fill(check, -1);
+
+            Queue<Integer> queue = new ArrayDeque<>();
+            queue.add(1);
+            check[1] = 0;
+            int answer = 0;
+
+            while(!queue.isEmpty()){
+                int cur = queue.poll();
+
+                if(check[cur] >= 2) continue;
+
+                for(int i : list[cur]){
+                    if(check[i] == -1){
+                        answer++;
+                        queue.add(i);
+                        check[i] = check[cur] +1;
+                    }
                 }
             }
+
+            System.out.println(answer);
+
+
+
         }
 
-        return cnt;
-
     }
-}
