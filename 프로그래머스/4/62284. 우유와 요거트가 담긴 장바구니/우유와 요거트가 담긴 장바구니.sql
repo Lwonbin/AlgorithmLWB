4eb072b4a8abb1124642a96064a92b02994dcf05
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-03-31 19:06:24 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,4 +1,8 @@
 -- 코드를 입력하세요
-SELECT DISTINCT(A.CART_ID) from CART_PRODUCTS A join (Select CART_ID, Name from CART_PRODUCTS) B on A.CART_ID = B.CART_ID
+SELECT A.CART_ID from CART_PRODUCTS A join CART_PRODUCTS B on A.CART_ID = B.CART_ID
 
 where A.NAME = "MILK" AND B.NAME = "Yogurt"
+
+group by A.CART_ID
+
+order by A.CART_ID
