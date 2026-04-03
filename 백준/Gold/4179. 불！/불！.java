bf6c94cac8a78d464791f0f9a6393c0f109c928b
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-04-03 17:54:43 +0900

    [Gold III] Title: 불!, Time: 528 ms, Memory: 53400 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -1,107 +1,115 @@
-import java.io.*;
-import java.util.*;
+    import java.io.*;
+    import java.sql.SQLOutput;
+    import java.util.*;
 
-public class Main {
-    static int R, C;
-    static char[][] map;
-    static int[][] jihun;
-    static int[][] fire;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static int[] WJ = new int[2];
-    static int[] WF = new int[2];
-    static int count = 0;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public class Main {
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        map = new char[R][C];
-        jihun = new int[R][C];
-        fire = new int[R][C];
+        static char[][] map;
+        static boolean[][] visited;
+        static int[][] fire;
+
+        static int R,C;
+        static int[] dx = {-1,1,0,0};
+        static int[] dy = {0,0,-1,1};
+
+        public static void main(String[] args) throws IOException {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
-        Queue<int[]> fireQueue = new LinkedList<>();
-        for(int i=0; i<R; i++){
-            String str = br.readLine();
-            for(int j=0; j<C; j++){
-                map[i][j] = str.charAt(j);
-                if(str.charAt(j) == 'J'){
-                    WJ[0] = i;
-                    WJ[1] = j;
-                }else if(str.charAt(j) == 'F'){
-                    fire[i][j] = 1;
-                    fireQueue.add(new int[] {i,j});
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-                }
+            R = Integer.parseInt(st.nextToken());
+            C = Integer.parseInt(st.nextToken());
+
+            map = new char[R][C];
+            visited = new boolean[R][C];
+            fire = new int[R][C];
+
+            for(int i=0; i<R; i++){
+                Arrays.fill(fire[i], -1);
             }
-        }
 
-        fireBfs(fireQueue);
-        jihunBfs(WJ[0], WJ[1]);
+            int startX = 0;
+            int startY = 0;
 
 
-    }
+            Queue<int[]> fireQueue = new ArrayDeque<>();
 
-    static void fireBfs(Queue<int[]> queue){
-        while(!queue.isEmpty()){
-            int[] cur = queue.poll();
-            int curX = cur[0];
-            int curY = cur[1];
-
-            for(int i=0; i<4; i++){
-                int nx = curX + dx[i];
-                int ny = curY + dy[i];
-
-                if(nx >= 0 && ny >= 0 && nx < R && ny < C){
-                    if(fire[nx][ny]==0 && map[nx][ny] == '.'){
-                        fire[nx][ny] = fire[curX][curY] + 1;
-                        queue.add(new int[] {nx,ny});
-
+            for(int i=0; i<R; i++){
+                String str = br.readLine();
+                for(int j=0; j<C; j++){
+                    map[i][j] = str.charAt(j);
+                    if(str.charAt(j) == 'F'){
+                        fireQueue.add(new int[]{i,j});
+                        fire[i][j] = 0;
+                    }else if(str.charAt(j) == 'J'){
+                        startX = i;
+                        startY = j;
                     }
                 }
             }
 
+            while(!fireQueue.isEmpty()){
+                int[] cur = fireQueue.poll();
+                int curX = cur[0];
+                int curY = cur[1];
 
-        }
-    }
+                for(int i=0; i<4; i++){
+                    int nx = curX + dx[i];
+                    int ny = curY + dy[i];
 
-    static void jihunBfs(int x, int y){
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {x, y});
-        jihun[x][y] = 1;
-
-        while(!queue.isEmpty()){
-            int[] cur = queue.poll();
-            int curX = cur[0];
-            int curY = cur[1];
-
-            for(int i=0; i<4; i++){
-                int nx = curX + dx[i];
-                int ny = curY + dy[i];
-
-                if(nx >= 0 && ny >= 0 && nx < R && ny < C){
-                    if(jihun[nx][ny]==0 && map[nx][ny] == '.'){
-                        int nextTime = jihun[curX][curY] + 1;
-                        if(fire[nx][ny] == 0 || nextTime < fire[nx][ny]){
-                            jihun[nx][ny] = nextTime;
-                            queue.add(new int[] {nx,ny});
-                        }else{
-                            jihun[nx][ny] = 0;
+                    if(nx>=0 && ny>=0 && nx<R && ny<C){
+                        if(fire[nx][ny] == -1 && map[nx][ny] != '#'){
+                            fire[nx][ny] = fire[curX][curY] +1;
+                            fireQueue.add(new int[]{nx,ny});
                         }
                     }
                 }
-
             }
 
-            if(curX == 0 || curY == 0 || curX == R-1 || curY == C-1){
-                System.out.println(jihun[curX][curY]);
-                System.exit(0);
+            visited = new boolean[R][C];
+            int answer = bfs(startX, startY, 0);
+            if(answer == -1){
+                System.out.println("IMPOSSIBLE");
+            }else{
+                System.out.println(answer+1) ;
             }
+
         }
 
-        System.out.println("IMPOSSIBLE");
+        static int bfs(int x, int y, int count){
+            Queue<int[]> queue = new ArrayDeque<>();
+            queue.add(new int[]{x,y, count});
+            visited[x][y] = true;
+
+            while(!queue.isEmpty()){
+                int[] cur = queue.poll();
+                int curX = cur[0];
+                int curY = cur[1];
+                int curCount = cur[2];
+
+                if(curX == 0 || curY ==0 || curX ==R-1 || curY == C-1){
+                    return curCount;
+                }
+
+                for(int i=0; i<4; i++){
+                    int nx = curX + dx[i];
+                    int ny = curY + dy[i];
+                    if(nx>=0 && ny>=0 && nx<R && ny<C){
+                        if(!visited[nx][ny] && map[nx][ny] == '.'){
+                            if(fire[nx][ny]-1 > curCount || fire[nx][ny] == -1){
+                                queue.add(new int[]{nx,ny, curCount+1});
+                                visited[nx][ny] = true;
+                            }
+                        }
+                    }
+                }
+            }
+            return -1;
+
+        }
+
+
     }
-}
