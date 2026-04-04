1495ec1eab7ef7bdeaf9119bc159f80223676972
Lwonbin <128762057+Lwonbin@users.noreply.github.com>
2026-04-04 11:27:25 +0900

    [level 1] Title: 신고 결과 받기, Time: 306.13 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,42 +1,42 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
         
-        int[] answer = new int[id_list.length];
-        boolean[][] check = new boolean[id_list.length][id_list.length];
-        HashMap<String, Integer> map = new HashMap<>();
-        int[] count = new int[id_list.length];
-        
-        
-        for(int i=0; i<id_list.length; i++){
-            map.put(id_list[i],i);
-        }
-        
-        
+        HashMap<String, Integer> Tomap = new HashMap<>();
+        HashMap<String, List<String>> Frommap = new HashMap<>();
+        answer = new int[id_list.length];
         
         for(int i=0; i<report.length; i++){
-            String[] sendReceive = report[i].split(" ");
-            String send = sendReceive[0];
-            String receive = sendReceive[1];
-    
-            if(!check[map.get(send)][map.get(receive)]){
-                count[map.get(receive)]++;
-                check[map.get(send)][map.get(receive)]=true;
+            String[] cur = report[i].split(" ");
+            String from = cur[0];
+            String to = cur[1];
+            
+            Frommap.putIfAbsent(from, new ArrayList<>());
+            
+            if(!Frommap.get(from).contains(to)){
+            Frommap.get(from).add(to);
+            Tomap.put(to, Tomap.getOrDefault(to, 0) + 1);
             }
         }
-        
-        
-        //count 에는 1202가 들어가있게 된다.
         
         for(int i=0; i<id_list.length; i++){
-            int validCount = 0;
-            for(int j=0; j<id_list.length; j++){
-                if(check[i][j] == true && count[j]>=k){
-                    validCount++;
+            String cur = id_list[i];
+            int count = 0;
+            
+            List<String> target = Frommap.getOrDefault(cur, new ArrayList<>());
+            
+            for(String str : target){
+                if(Tomap.getOrDefault(str, 0) >= k){
+                    count++;
                 }
             }
-            answer[i] = validCount;
+            answer[i] = count;
+            
+            
         }
+        
         
         return answer;
     }
